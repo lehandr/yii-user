0bb903518b1f93c8ee058808cead22fc81e12921
Aleksey Naydenko <aleksey.naydenko@virtualhealth.com>
2014-03-25 17:26:10 +0200

    Fix form search style

--- views/admin/_search.php
+++ views/admin/_search.php
@@ -7,57 +7,57 @@
 )); ?>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'id',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'id',array('class'=>'control-label')); ?>
             <?php echo $form->textField($model,'id'); ?>
         </div>
     </div>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'username',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'username',array('class'=>'control-label')); ?>
             <?php echo $form->textField($model,'username',array('size'=>20,'maxlength'=>20)); ?>
         </div>
     </div>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'email',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'email',array('class'=>'control-label')); ?>
             <?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>128)); ?>
         </div>
     </div>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'activkey',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'activkey',array('class'=>'control-label')); ?>
             <?php echo $form->textField($model,'activkey',array('size'=>60,'maxlength'=>128)); ?>
         </div>
     </div>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'create_at',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'create_at',array('class'=>'control-label')); ?>
             <?php echo $form->textField($model,'create_at'); ?>
         </div>
     </div>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'lastvisit_at',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'lastvisit_at',array('class'=>'control-label')); ?>
             <?php echo $form->textField($model,'lastvisit_at'); ?>
         </div>
     </div>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'superuser',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'superuser',array('class'=>'control-label')); ?>
             <?php echo $form->dropDownList($model,'superuser',$model->itemAlias('AdminStatus')); ?>
         </div>
     </div>
 
     <div class="control-group">
+        <?php echo $form->labelEx($model,'status',array('class'=>'control-label')); ?>
         <div class="controls">
-            <?php echo $form->labelEx($model,'status',array('class'=>'control-label')); ?>
             <?php echo $form->dropDownList($model,'status',$model->itemAlias('UserStatus')); ?>
         </div>
     </div>
